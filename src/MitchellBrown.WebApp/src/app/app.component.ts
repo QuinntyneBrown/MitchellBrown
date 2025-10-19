20bd24874231e179eced04535e3f43735f992c96
Quinntyne Brown <quinntynebrown@gmail.com>
2025-10-18 22:27:14 -0400

    update

--- src/MitchellBrown.WebApp/src/app/app.component.ts
+++ src/MitchellBrown.WebApp/src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  standalone: true,
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'MitchellBrown.WebApp';
+
 }
