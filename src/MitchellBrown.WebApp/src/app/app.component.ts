db4d75328bee46a2703b188e713a325de3509b39
Quinntyne Brown <quinntynebrown@gmail.com>
2025-10-18 22:32:30 -0400

    update

--- src/MitchellBrown.WebApp/src/app/app.component.ts
+++ src/MitchellBrown.WebApp/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { Observable } from 'rxjs';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
+  imports: [
+    HeaderComponent,
+    RouterOutlet
+],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  viewModel$: Observable<{}> = null!;
 }
